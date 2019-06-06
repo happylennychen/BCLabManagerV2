19ecae6f54d2df02387675c228c12ae47d96b304
happylennychen <happylennychen@gmail.com>
2019-06-06 18:14:29 +0800

    Add files via upload

---
+++ BCLabManagerV2/Model/Assets.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace O2Micro.BCLabManager.Model
+{
+    public enum AssetStatusEnum
+    {
+        IDLE,
+        USING,
+    }
+    public class AssetClass
+    {
+        private static Int32 nextID = 1;
+        private Int32 NextID
+        {
+            get
+            {
+                nextID += 1;
+                return nextID - 1;
+            }
+        }
+        public Int32 AssetID { get; set; }
+
+        private AssetStatusEnum status = new AssetStatusEnum();
+        public AssetStatusEnum Status
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                if (value != status)
+                    status = value;
+                else
+                {
+                    //Todo: throw exception here
+                }
+            }
+        }
+
+        public class Record
+        {
+            DateTime Timestamp;
+            AssetStatusEnum Status;
+            String ProgramName;
+            String SubProgramName;
+
+            public Record(DateTime Timestamp, AssetStatusEnum Status, String ProgramName, String SubProgramName)
+            {
+                this.Timestamp = Timestamp;
+                this.Status = Status;
+                this.ProgramName = ProgramName;
+                this.SubProgramName = SubProgramName;
+            }
+        }
+        public List<Record> Records { get; set; }
+
+        public AssetClass()
+        {
+            this.AssetID = NextID;
+            this.Status = AssetStatusEnum.IDLE;
+            Records = new List<Record>();
+        }
+
+        public void UpdateRecords(DateTime Timestamp, AssetStatusEnum Status, String ProgramName, String SubProgramName)
+        {
+            Records.Add(new Record(Timestamp, Status, ProgramName, SubProgramName));
+        }
+    }
+    public class BatteryTypeClass
+    {
+        private static Int32 nextID = 1;
+        private Int32 NextID
+        {
+            get
+            {
+                nextID += 1;
+                return nextID - 1;
+            }
+        }
+        public Int32 BatteryTypeID { get; set; }
+        public String Manufactor { get; set; }
+        public String Name { get; set; }
+        public String Material { get; set; }
+        public Int32 LimitedChargeVoltage { get; set; }
+        public Int32 RatedCapacity { get; set; }
+        public Int32 NominalVoltage { get; set; }
+        public Int32 TypicalCapacity { get; set; }
+        public Int32 CutoffDischargeVoltage { get; set; }
+        //public List<Battery> Batteries { get; set; }
+        //public List<Program> TestPlans { get; set; }
+
+        public BatteryTypeClass()
+        { }
+
+        public BatteryTypeClass(Int32 BatteryTypeID, String Manufactor, String Name, String Material, Int32 LimitedChargeVoltage, Int32 RatedCapacity, Int32 NominalVoltage, Int32 TypicalCapacity, Int32 CutoffDischargeVoltage)
+        {
+            this.BatteryTypeID = BatteryTypeID;
+            this.Manufactor = Manufactor;
+            this.Name = Name;
+            this.Material = Material;
+            this.LimitedChargeVoltage = LimitedChargeVoltage;
+            this.RatedCapacity = RatedCapacity;
+            this.NominalVoltage = NominalVoltage;
+            this.TypicalCapacity = TypicalCapacity;
+            this.CutoffDischargeVoltage = CutoffDischargeVoltage;
+        }
+        public BatteryTypeClass(String Manufactor, String Name, String Material, Int32 LimitedChargeVoltage, Int32 RatedCapacity, Int32 NominalVoltage, Int32 TypicalCapacity, Int32 CutoffDischargeVoltage)
+        {
+            this.BatteryTypeID = NextID;
+            this.Manufactor = Manufactor;
+            this.Name = Name;
+            this.Material = Material;
+            this.LimitedChargeVoltage = LimitedChargeVoltage;
+            this.RatedCapacity = RatedCapacity;
+            this.NominalVoltage = NominalVoltage;
+            this.TypicalCapacity = TypicalCapacity;
+            this.CutoffDischargeVoltage = CutoffDischargeVoltage;
+        }
+    }
+
+    public class BatteryClass : AssetClass
+    {
+        private static Int32 nextID = 1;
+        private Int32 NextID
+        {
+            get
+            {
+                nextID += 1;
+                return nextID - 1;
+            }
+        }
+        public Int32 BatteryID { get; set; }
+        public String Name { get; set; }
+        public BatteryTypeClass BatteryType { get; set; }
+        public Double CycleCount { get; set; }
+        //public AssestStatus Stauts { get; set; }
+
+        public BatteryClass()
+        { }
+
+        public BatteryClass(Int32 BatteryID, String Name, BatteryTypeClass BatteryType, Double CycleCount = 0)
+        {
+            this.BatteryID = BatteryID;
+            this.Name = Name;
+            this.BatteryType = BatteryType;
+            this.CycleCount = CycleCount;
+        }
+        public BatteryClass(String Name, BatteryTypeClass BatteryType, Double CycleCount = 0)
+        {
+            this.BatteryID = NextID;
+            this.Name = Name;
+            this.BatteryType = BatteryType;
+            this.CycleCount = CycleCount;
+        }
+    }
+
+
+    public class TesterClass
+    {
+        private static Int32 nextID = 1;
+        private Int32 NextID
+        {
+            get
+            {
+                nextID += 1;
+                return nextID - 1;
+            }
+        }
+        public Int32 TesterID { get; set; }
+        public String Manufactor { get; set; }
+        public String Name { get; set; }
+        public List<TesterChannelClass> TesterChannels { get; set; }
+
+        public TesterClass(Int32 TesterID, String Manufactor, String Name, List<TesterChannelClass> TesterChannels)
+        {
+            this.TesterID = TesterID;
+            this.Manufactor = Manufactor;
+            this.Name = Name;
+            this.TesterChannels = TesterChannels;
+        }
+        public TesterClass(String Manufactor, String Name, List<TesterChannelClass> TesterChannels)
+        {
+            this.TesterID = NextID;
+            this.Manufactor = Manufactor;
+            this.Name = Name;
+            this.TesterChannels = TesterChannels;
+        }
+    }
+    public class TesterChannelClass : AssetClass
+    {
+        private static Int32 nextID = 1;
+        private Int32 NextID
+        {
+            get
+            {
+                nextID += 1;
+                return nextID - 1;
+            }
+        }
+        public Int32 TesterChannelID { get; set; }
+        public TesterClass Tester { get; set; }
+
+        public TesterChannelClass(Int32 TesterChannelID, TesterClass Tester)
+        {
+            this.TesterChannelID = TesterChannelID;
+            this.Tester = Tester;
+        }
+        public TesterChannelClass(TesterClass Tester)
+        {
+            this.TesterChannelID = NextID;
+            this.Tester = Tester;
+        }
+    }
+
+    public class ChamberClass : AssetClass
+    {
+        private static Int32 nextID = 1;
+        private Int32 NextID
+        {
+            get
+            {
+                nextID += 1;
+                return nextID - 1;
+            }
+        }
+        public Int32 ChamberID { get; set; }
+        public String Manufactor { get; set; }
+        public String Name { get; set; }
+        public Double LowestTemperature { get; set; }
+        public Double HighestTemperature { get; set; }
+
+        public ChamberClass(Int32 ChamberID, String Manufactor, String Name, Double LowestTemperature, Double HighestTemperature)
+        {
+            this.ChamberID = ChamberID;
+            this.Manufactor = Manufactor;
+            this.Name = Name;
+            this.LowestTemperature = LowestTemperature;
+            this.HighestTemperature = HighestTemperature;
+        }
+        public ChamberClass(String Manufactor, String Name, String TemperatureRange)
+        {
+            this.ChamberID = NextID;
+            this.Manufactor = Manufactor;
+            this.Name = Name;
+            this.LowestTemperature = LowestTemperature;
+            this.HighestTemperature = HighestTemperature;
+        }
+    }
+}
+
---
+++ BCLabManagerV2/Model/Programs.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace O2Micro.BCLabManager.Model
+{
+    public enum TestCountEnum
+    {
+        One,
+        Two,
+    }
+    public enum TestStatus
+    {
+        Waiting,
+        Executing,
+        Completed,
+        Invalid,
+        Abandoned,
+    }
+    public class RawDataClass
+    { }
+    public class TestClass
+    {
+        private static Int32 nextID = 1;
+        private Int32 NextID
+        {
+            get
+            {
+                nextID += 1;
+                return nextID - 1;
+            }
+        }
+        public Int32 TestID { get; set; }
+        private TestStatus status = TestStatus.Waiting;
+        public TestStatus Status
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                if (value != status)
+                {
+                    status = value;
+                    OnRasieStatusChangedEvent();
+                }
+            }
+        }
+        public BatteryClass Battery { get; set; }
+        //public TesterClass Tester { get; set; }
+        public TesterChannelClass TesterChannel { get; set; }
+        public String Steps { get; set; }
+        public ChamberClass Chamber { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public String Description { get; set; }
+        public RawDataClass RawData { get; set; }
+        public SubProgramClass SubProgram { get; set; }
+
+        public event EventHandler StatusChanged;
+
+        protected virtual void OnRasieStatusChangedEvent()
+        {
+            EventHandler handler = StatusChanged;
+
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+        public TestClass(SubProgramClass SubProgram)
+        {
+            this.TestID = NextID;
+            this.SubProgram = SubProgram;
+            this.Status = TestStatus.Waiting;
+            this.Description = String.Empty;
+            this.Battery = null;
+            this.TesterChannel = null;
+            this.Chamber = null;
+            //this.StartTime = null;
+            //this.EndTime = DateTime.;
+        }
+
+        public void AssignAssets(BatteryClass Battery, ChamberClass Chamber, TesterChannelClass TesterChannel)
+        {
+
+            if (this.Status != TestStatus.Waiting)
+            {
+                //Todo: prompt warning message
+                System.Windows.MessageBox.Show("Only waiting task can be assign assets to!");
+                return;
+            }
+
+            if (Battery.Status != AssetStatusEnum.IDLE)
+            {
+                //Todo: prompt warning message
+                System.Windows.MessageBox.Show("Battery is in use!");
+                return;
+            }
+
+            if (Chamber != null)
+            {
+                if (Chamber.Status != AssetStatusEnum.IDLE)
+                {
+                    //Todo: prompt warning message
+                    System.Windows.MessageBox.Show("Chamber is in use!");
+                    return;
+                }
+                if (this.RequestedRecipe.Recipe.ChamberRecipe == null)
+                {
+                    //Todo: prompt warning message but do not return
+                    System.Windows.MessageBox.Show("No chamber recipe but chamber is provided!");
+                    //return;
+                }
+            }
+            else if (this.RequestedRecipe.Recipe.ChamberRecipe != null)
+            {
+                //Todo: prompt warning message
+                System.Windows.MessageBox.Show("No chamber to be assigned to chamber recipe!");
+                return;
+            }
+
+            if (TesterChannel.Status != AssetStatusEnum.IDLE)
+            {
+                //Todo: prompt warning message
+                System.Windows.MessageBox.Show("Test Channel is in use!");
+                return;
+            }
+
+            if (TesterChannel.Tester != this.RequestedRecipe.Recipe.TesterRecipe.Tester)
+            {
+                //Todo: prompt warning message
+                System.Windows.MessageBox.Show("Recipe and Tester are not compatible!");
+                return;
+            }
+
+            if (Battery.BatteryType != this.RequestedRecipe.Recipe.TesterRecipe.BatteryType)
+            {
+                //Todo: prompt warning message
+                System.Windows.MessageBox.Show("Recipe and Battery are not compatible!");
+                return;
+            }
+
+            this.Battery = Battery;
+            Battery.Status = AssetStatusEnum.ASSIGNED;
+            if (this.RequestedRecipe.Recipe.ChamberRecipe != null) //If there is no chamber recipe, then we don't need to assign chamber at all.
+            {
+                this.Chamber = Chamber;
+                Chamber.Status = AssetStatusEnum.ASSIGNED;
+            }
+            this.TesterChannel = TesterChannel;
+            TesterChannel.Status = AssetStatusEnum.ASSIGNED;
+            this.Status = TestStatus.Ready;
+        }
+
+        public void Execute(DateTime StartTime)
+        {
+            if (this.Status == TestStatus.Ready)
+            {
+                this.StartTime = StartTime;
+                Battery.Status = AssetStatusEnum.USING;
+                Battery.UpdateRecords(StartTime, Battery.Status);
+                if (Chamber != null)
+                {
+                    Chamber.Status = AssetStatusEnum.USING;
+                    Chamber.UpdateRecords(StartTime, Chamber.Status);
+                }
+                TesterChannel.Status = AssetStatusEnum.USING;
+                TesterChannel.UpdateRecords(StartTime, TesterChannel.Status);
+                this.Status = TestStatus.Executing;
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Only ready tasks can be executed!");
+            }
+        }
+
+        public void Commit(TestStatus Status, DateTime EndTime, String Description = "")  //Need to check the Executor Status to make sure it is executing
+        {
+            if (Status != TestStatus.Completed && Status != TestStatus.Invalid)
+            {
+                System.Windows.MessageBox.Show("Only Completed and Invalid are available status to be commited");
+                return;
+            }
+            if (this.Status == TestStatus.Executing)
+            {
+                this.Description = Description;
+                this.EndTime = EndTime;
+                if (Chamber != null)
+                {
+                    Chamber.Status = AssetStatusEnum.IDLE;
+                    Chamber.UpdateRecords(StartTime, Chamber.Status);
+                }
+                TesterChannel.Status = AssetStatusEnum.IDLE;
+                TesterChannel.UpdateRecords(StartTime, TesterChannel.Status);
+                Battery.Status = AssetStatusEnum.IDLE;
+                Battery.UpdateRecords(StartTime, Battery.Status);
+                this.Status = Status;   //this has to be the last assignment because it will raise StatusChanged event so that duration will be updated using StartTime and EndTime
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Only executing tasks can be commited!");
+            }
+        }
+
+        public void Abandon()
+        {
+            if (this.Status == TestStatus.Abandoned)
+            {
+                System.Windows.MessageBox.Show("Abandoning Abandoned tasks is meaningless!");
+                return;
+            }
+            else if (this.Status == TestStatus.Executing)
+            {
+                System.Windows.MessageBox.Show("Commit running task before abandon it!");
+                return;
+            }
+            if (this.Battery != null)
+                this.Battery.Status = AssetStatusEnum.IDLE;
+            if (this.Chamber != null)
+                this.Chamber.Status = AssetStatusEnum.IDLE;
+            if (this.TesterChannel != null)
+                this.TesterChannel.Status = AssetStatusEnum.IDLE;
+            this.Status = TestStatus.Abandoned;
+        }
+
+        public void Invalidate()
+        {
+            if (this.Status == TestStatus.Completed || this.Status == TestStatus.Executing)
+            {
+                this.Battery.Status = AssetStatusEnum.IDLE;
+                if (this.Chamber != null)
+                    this.Chamber.Status = AssetStatusEnum.IDLE;
+                this.TesterChannel.Status = AssetStatusEnum.IDLE;
+                this.Status = TestStatus.Invalid;
+                this.RequestedRecipe.AddExecutor();
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Only completed or executing tasks can be Invalidated!");
+            }
+        }
+    }
+    // Summary:
+    //     Represents a sub program which can be united to form a program
+    public class SubProgramClass
+    {
+        private static Int32 nextID = 1;
+        private Int32 NextID
+        {
+            get
+            {
+                nextID += 1;
+                return nextID - 1;
+            }
+        }
+        public Int32 SubProgramID { get; set; }
+        public String Name { get; set; }
+        public TestCountEnum TestCount { get; set; }
+        private TestClass[] test = new TestClass[2];
+        public TestClass[] Test 
+        {
+            get
+            { 
+                return test; 
+            }
+            set
+            {
+                test = value;
+            }
+        }
+
+        public SubProgramClass(String Name, TestCountEnum TestCount)
+        {
+            this.SubProgramID = NextID;
+            this.Name = Name;
+            this.TestCount = TestCount;
+        }
+
+        public SubProgramClass(SubProgramClass old)
+        {
+            this.SubProgramID = NextID;
+            this.Name = old.Name;
+            this.TestCount = old.TestCount;
+        }
+    }
+
+    public class ProgramClass
+    {
+        private static Int32 nextID = 1;
+        private Int32 NextID
+        {
+            get
+            {
+                nextID += 1;
+                return nextID - 1;
+            }
+        }
+        public Int32 ProgramID { get; set; }
+        public String Name { get; set; }
+        public String Requester { get; set; }
+        public DateTime RequestDate { get; set; }
+        public String Description { get; set; }
+        //public Int32 Priority { get; set; }
+        public DateTime CompleteDate { get; set; }
+        public List<SubProgramClass> SubPrograms { get; set; }
+
+        public ProgramClass()
+        { }
+
+        public ProgramClass(String Name, String Requester, DateTime RequestDate, List<SubProgramClass> SubPrograms)
+        {
+            this.ProgramID = NextID;
+            this.Name = Name;
+            this.Requester = Requester;
+            this.RequestDate = RequestDate;
+            this.SubPrograms = SubPrograms;
+        }
+    }
+}
