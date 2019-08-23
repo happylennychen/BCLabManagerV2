6652da04af66ede2b579c1f085319230bb3afe24
leon.chen <happylennychen@outlook.com>
2019-08-23 17:34:58 +0800

    Using Bootstrapper

--- BCLabManagerV2/App.xaml.cs
+++ BCLabManagerV2/App.xaml.cs
@@ -12,5 +12,12 @@ namespace BCLabManager
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Bootstrapper bs = new Bootstrapper();
+            bs.Run();
+        }
     }
 }
---
+++ BCLabManagerV2/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿//using CommonServiceLocator;
+using Prism.Unity;
+using Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BCLabManager
+{
+    public class Bootstrapper:UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            //return ServiceLocator.Current.GetInstance<MainWindow>();
+            return Container.Resolve<MainWindow>();
+            //return new MainWindow();
+        }
+        protected override void InitializeShell()
+        {
+            //Application.Current.MainWindow = Shell;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
