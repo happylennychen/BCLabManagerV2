44f7c37c3ee71306a0142eb8881d2b27a82f4390
leon.chen <happylennychen@outlook.com>
2019-09-19 19:56:37 +0800

    Color pattern

--- BCLabManagerV2/DatabasePopulator.cs
+++ BCLabManagerV2/DatabasePopulator.cs
@@ -18,11 +18,93 @@ namespace BCLabManager
             PopulateOperations();
         }
 
-        private static void PopulateOperations()
+        private static void PopulateAssets()
         {
-            //throw new NotImplementedException();
+            PopulateBatteryTypes();
+            PopulateBatteries();
+            PopulateTesters();
+            PopulateChannels();
+            PopulateChambers();
         }
 
+        private static void PopulateBatteryTypes()
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                BatteryTypeClass btc = new BatteryTypeClass()
+                {
+                    Name = "BLP663",
+                    Manufactor = "Oppo"
+                };
+                dbContext.BatteryTypes.Add(btc);
+                dbContext.SaveChanges();
+            }
+        }
+
+        private static void PopulateBatteries()
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    BatteryClass bc = new BatteryClass()
+                    {
+                        Name = $"BLP663-{i + 1}",
+                        BatteryType = dbContext.BatteryTypes.SingleOrDefault(o => o.Name == "BLP663"),
+                        CycleCount = 0,
+                        Status = AssetStatusEnum.IDLE
+                    };
+                    dbContext.Batteries.Add(bc);
+                }
+                dbContext.SaveChanges();
+            }
+        }
+
+        private static void PopulateTesters()
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                TesterClass tc = new TesterClass()
+                {
+                    Name = "17200",
+                    Manufactor = "Chroma"
+                };
+                dbContext.Testers.Add(tc);
+                dbContext.SaveChanges();
+            }
+        }
+
+        private static void PopulateChannels()
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    ChannelClass chc = new ChannelClass()
+                    {
+                        Name = $"Channel-{i + 1}",
+                        Tester = dbContext.Testers.SingleOrDefault(o => o.Name == "17200"),
+                        Status = AssetStatusEnum.IDLE
+                    };
+                    dbContext.Channels.Add(chc);
+                }
+                dbContext.SaveChanges();
+            }
+        }
+
+        private static void PopulateChambers()
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                ChamberClass cmc = new ChamberClass()
+                {
+                    Name = "PUL-80",
+                    Manufactor = "Hongzhan"
+                };
+                dbContext.Chambers.Add(cmc);
+                dbContext.SaveChanges();
+            }
+        }
         private static void PopulatePrograms()
         {
             PopulateChargeTemperatures();
@@ -699,94 +781,6 @@ namespace BCLabManager
             }
         }
 
-        private static void PopulateAssets()
-        {
-            PopulateBatteryTypes();
-            PopulateBatteries();
-            PopulateTesters();
-            PopulateChannels();
-            PopulateChambers();
-        }
-
-        private static void PopulateBatteryTypes()
-        {
-            using (var dbContext = new AppDbContext())
-            {
-                BatteryTypeClass btc = new BatteryTypeClass()
-                {
-                    Name = "BLP663",
-                    Manufactor = "Oppo"
-                };
-                dbContext.BatteryTypes.Add(btc);
-                dbContext.SaveChanges();
-            }
-        }
-
-        private static void PopulateBatteries()
-        {
-            using (var dbContext = new AppDbContext())
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    BatteryClass bc = new BatteryClass()
-                    {
-                        Name = $"BLP663-{i + 1}",
-                        BatteryType = dbContext.BatteryTypes.SingleOrDefault(o => o.Name == "BLP663"),
-                        CycleCount = 0,
-                        Status = AssetStatusEnum.IDLE
-                    };
-                    dbContext.Batteries.Add(bc);
-                }
-                dbContext.SaveChanges();
-            }
-        }
-
-        private static void PopulateTesters()
-        {
-            using (var dbContext = new AppDbContext())
-            {
-                TesterClass tc = new TesterClass()
-                {
-                    Name = "17200",
-                    Manufactor = "Chroma"
-                };
-                dbContext.Testers.Add(tc);
-                dbContext.SaveChanges();
-            }
-        }
-
-        private static void PopulateChannels()
-        {
-            using (var dbContext = new AppDbContext())
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    ChannelClass chc = new ChannelClass()
-                    {
-                        Name = $"Channel-{i + 1}",
-                        Tester = dbContext.Testers.SingleOrDefault(o => o.Name == "17200"),
-                        Status = AssetStatusEnum.IDLE
-                    };
-                    dbContext.Channels.Add(chc);
-                }
-                dbContext.SaveChanges();
-            }
-        }
-
-        private static void PopulateChambers()
-        {
-            using (var dbContext = new AppDbContext())
-            {
-                ChamberClass cmc = new ChamberClass()
-                {
-                    Name = "PUL-80",
-                    Manufactor = "Hongzhan"
-                };
-                dbContext.Chambers.Add(cmc);
-                dbContext.SaveChanges();
-            }
-        }
-
         private static SubProgramTemplate GetSubProgramTemplateById(AppDbContext dbContext, int id)
         {
             var subtemplate = dbContext.SubProgramTemplates.SingleOrDefault(o => o.Id == id);
@@ -803,6 +797,45 @@ namespace BCLabManager
                 .Reference(o => o.DischargeCurrent)
                 .Load();
             return subtemplate;
+        }
+
+
+        private static void PopulateOperations()
+        {
+            TestRecordClass tr;
+            SubProgramClass sub;
+            ProgramClass pro;
+            using (var dbContext = new AppDbContext())
+            {
+                tr = dbContext.TestRecords.SingleOrDefault(i => i.Id == 4);
+                sub = dbContext.SubPrograms.SingleOrDefault(o => o.Id == 1);
+                dbContext.Entry(sub)
+                    .Reference(o => o.ChargeTemperature)
+                    .Load();
+                dbContext.Entry(sub)
+                    .Reference(o => o.ChargeCurrent)
+                    .Load();
+                dbContext.Entry(sub)
+                    .Reference(o => o.DischargeTemperature)
+                    .Load();
+                dbContext.Entry(sub)
+                    .Reference(o => o.DischargeCurrent)
+                    .Load();
+                pro = dbContext.Programs.SingleOrDefault(o => o.Id == 1);
+                tr.BatteryTypeStr = dbContext.BatteryTypes.SingleOrDefault(o=>o.Id == 1).Name;
+                tr.BatteryStr = dbContext.Batteries.SingleOrDefault(o => o.Id == 1).Name;
+                tr.ChamberStr = dbContext.Chambers.SingleOrDefault(o => o.Id == 1).Name;
+                tr.TesterStr = dbContext.Testers.SingleOrDefault(o => o.Id == 1).Name;
+                tr.ChannelStr = dbContext.Channels.SingleOrDefault(o => o.Id == 1).Name;
+                tr.StartTime = DateTime.Parse("2019/3/7  17:05:58");
+                tr.Steps = "";
+                tr.Status = TestStatus.Executing;
+                tr.AssignedBattery = dbContext.Batteries.SingleOrDefault(o => o.Id == 1);
+                tr.AssignedChamber = dbContext.Chambers.SingleOrDefault(o => o.Id == 1);
+                tr.AssignedChannel = dbContext.Channels.SingleOrDefault(o => o.Id == 1);
+                dbContext.SaveChanges();
+            }
+            tr.AssetsExecute(tr.AssignedBattery, tr.AssignedChamber, tr.AssignedChannel, tr.Steps, tr.StartTime, pro.Name, $"{sub.ChargeTemperature.Name} {sub.ChargeCurrent} charge, {sub.DischargeTemperature} {sub.DischargeCurrent} discharge");
         }
     }
 }
