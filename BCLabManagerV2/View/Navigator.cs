eafbacf1926685baccfc9dfdab3fbab7f2f96164
leon.chen <happylennychen@outlook.com>
2019-09-19 09:36:36 +0800

    Add Navigator, we did not add it before because we execute add operation in a sub folder

---
+++ BCLabManagerV2/View/Navigator.cs
@@ -0,0 +1,50 @@
+﻿using BCLabManager.Model;
+using BCLabManager.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCLabManager.View
+{
+    public static class Navigator
+    {
+        private static MainWindow _mainWindow;
+        public static void Initialize(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+        public static void SetMainTabIndex(int i)   //0:Dashboard 1:Assets 2:Sub Program Template 3:Programs
+        {
+            _mainWindow.MainTab.SelectedIndex = i;
+        }
+        public static void SetSelectedTestRecord(int id)
+        {
+            foreach (var pro in _mainWindow.allProgramsViewModel.AllPrograms)
+                foreach (var sub in pro.SubPrograms)
+                {
+                    foreach(var tr in sub.Test1Records)
+                    {
+                        if (tr.Id == id)
+                        {
+                            _mainWindow.AllProgramsViewInstance.Programlist.SelectedItem = pro;
+                            _mainWindow.AllProgramsViewInstance.SubProgramlist.SelectedItem = sub;
+                            _mainWindow.AllProgramsViewInstance.FirstTestRecordList.SelectedItem = tr;
+                            return;
+                        }
+                    }
+                    foreach (var tr in sub.Test2Records)
+                    {
+                        if (tr.Id == id)
+                        {
+                            _mainWindow.AllProgramsViewInstance.Programlist.SelectedItem = pro;
+                            _mainWindow.AllProgramsViewInstance.SubProgramlist.SelectedItem = sub;
+                            _mainWindow.AllProgramsViewInstance.SecondTestRecordList.SelectedItem = tr;
+                            return;
+                        }
+                    }
+                }
+        }
+    }
+}
