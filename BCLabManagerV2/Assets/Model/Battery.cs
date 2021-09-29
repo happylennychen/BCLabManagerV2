3c08b06545a2b0d2e7957ce4d7a8413128b57c6b
Leon Chen(WH) <leon.chen@o2micro.com>
2021-09-29 12:05:34 +0800

    Use double navigation property

--- BCLabManagerV2/Assets/Model/Battery.cs
+++ BCLabManagerV2/Assets/Model/Battery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BCLabManager.Model
+﻿namespace BCLabManager.Model
 {
     public class Battery : Asset
     {
