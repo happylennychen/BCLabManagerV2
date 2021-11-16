14e9acf83c55520a2302cc9226bd365d9c76ea6b
Leon Chen(WH) <leon.chen@o2micro.com>
2021-11-16 16:42:53 +0800

    fixed project update error

--- BCLabManagerV2/DataAccess/ProjectRepository.cs
+++ BCLabManagerV2/DataAccess/ProjectRepository.cs
@@ -14,5 +14,20 @@ namespace BCLabManager.DataAccess
         {
 
         }
+
+        protected new void Update(Project prj)
+        {
+            var old_prj = _context.Projects.Single(o => o.Id == prj.Id);
+            old_prj.Name = prj.Name;
+            old_prj.AbsoluteMaxCapacity = prj.AbsoluteMaxCapacity;
+            old_prj.Customer = prj.Customer;
+            old_prj.CutoffDischargeVoltage = prj.CutoffDischargeVoltage;
+            old_prj.LimitedChargeVoltage = prj.LimitedChargeVoltage;
+            old_prj.VoltagePoints = prj.VoltagePoints;
+            old_prj.Description = prj.Description;
+            //old_prj.
+            var bt = _context.BatteryTypes.Single(o => o.Id == prj.BatteryType.Id);
+            old_prj.BatteryType = bt;
+        }
     }
 }
